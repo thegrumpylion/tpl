bf85d1f299d4f77d2eade8f263f70d24cc442663
Nikolas Sepos <nikolas.sepos@gmail.com>
2023-04-18 02:16:57 +0300

    git

--- gen.go
+++ gen.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -10,87 +12,114 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig"
+	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
 
 var genCmdArgs = struct {
-	Template string
-	Dir      string
+	Template   string
+	LeftDelim  string
+	RightDelim string
 }{}
 
 func init() {
 	genCmd.Flags().StringVarP(&genCmdArgs.Template, "template", "t", "", "template file")
 	genCmd.MarkFlagRequired("template")
-	genCmd.Flags().StringVarP(&genCmdArgs.Dir, "dir", "d", "", "directory to generate files in")
+	genCmd.Flags().StringVarP(&genCmdArgs.LeftDelim, "left-delimiter", "l", "{{{", "left delimiter")
+	genCmd.Flags().StringVarP(&genCmdArgs.RightDelim, "right-delimiter", "r", "}}}", "right delimiter")
 	rootCmd.AddCommand(genCmd)
 }
 
 var genCmd = &cobra.Command{
 	Use:  "gen",
 	RunE: runEgenCmd,
+	Args: cobra.ExactArgs(1),
 }
 
 func runEgenCmd(cmd *cobra.Command, args []string) error {
 
-	src := "templates/" + genCmdArgs.Template
-	trgt := genCmdArgs.Dir
-	modName := args[0]
+	u, err := url.Parse(genCmdArgs.Template)
+	if err != nil {
+		parts := strings.Split(genCmdArgs.Template, "/")
 
-	sprigFuncMap := sprig.GenericFuncMap()
+		if len(parts) == 1 {
+			return fmt.Errorf("template must be in the form of [<remote>/]<owner>/<repo> or a valid URL")
+		}
 
-	if err := os.MkdirAll(trgt, os.ModePerm); err != nil {
+		remote := "github.com"
+
+		if len(parts) == 3 {
+			remote = parts[0]
+			parts = parts[1:]
+		}
+
+		if len(parts) > 3 {
+			return fmt.Errorf("template must be in the form of [<remote>/]<owner>/<repo> or a valid URL")
+		}
+
+		u = &url.URL{
+			Scheme: "https",
+			Host:   remote,
+			Path:   strings.Join(parts, "/"),
+		}
+	}
+
+	src, err := cloneTemplate(u)
+	if err != nil {
 		return err
 	}
 
-	err := fs.WalkDir(tpls, src, func(path string, d fs.DirEntry, err error) error {
+	trgtDir := args[0]
+
+	sprigFuncMap := sprig.GenericFuncMap()
+
+	if err := os.MkdirAll(trgtDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	err = filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
 
 		if path == src {
 			return nil
 		}
 
 		trgtName := path[len(src)+1:]
-		// go embed limitation
-		if trgtName == "go.mod.tpl" {
-			trgtName = "go.mod"
+
+		if strings.HasPrefix(trgtName, ".git") {
+			return nil
 		}
 
-		nameTpl, err := template.New("").Delims("{{{", "}}}").Funcs(sprigFuncMap).Parse(trgtName)
+		nameTpl, err := template.New("").Delims(genCmdArgs.LeftDelim, genCmdArgs.RightDelim).Funcs(sprigFuncMap).Parse(trgtName)
 		if err != nil {
 			return err
 		}
 
 		var buf bytes.Buffer
-		err = nameTpl.Execute(&buf, tplContext{
-			Name: modName,
-			Org:  "thegrumpylion",
-		})
+		err = nameTpl.Execute(&buf, templateContext)
 		if err != nil {
 			return err
 		}
 
 		if d.IsDir() {
-			return os.MkdirAll(filepath.Join(trgt, buf.String()), os.ModePerm)
+			return os.MkdirAll(filepath.Join(trgtDir, buf.String()), os.ModePerm)
 		}
 
-		fileTplContent, err := tpls.ReadFile(path)
+		fileTplContent, err := ioutil.ReadFile(path)
 		if err != nil {
 			return err
 		}
 
-		fileTpl, err := template.New("").Delims("{{{", "}}}").Funcs(sprigFuncMap).Parse(string(fileTplContent))
+		fileTpl, err := template.New("").Delims(genCmdArgs.LeftDelim, genCmdArgs.RightDelim).Funcs(sprigFuncMap).Parse(string(fileTplContent))
 		if err != nil {
 			return err
 		}
 
-		f, err := os.Create(filepath.Join(trgt, buf.String()))
+		f, err := os.Create(filepath.Join(trgtDir, buf.String()))
 		if err != nil {
 			return err
 		}
 
-		err = fileTpl.Execute(f, tplContext{
-			Name: modName,
-			Org:  "thegrumpylion",
-		})
+		err = fileTpl.Execute(f, templateContext)
 		if err != nil {
 			return err
 		}
@@ -104,57 +133,59 @@ func runEgenCmd(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func getTemplateFolder(src string) (string, error) {
-	u, err := url.Parse(src)
-	if err != nil {
-		// is url
-		switch u.Scheme {
+func cloneTemplate(u *url.URL) (string, error) {
 
+	trgt := filepath.Join("~/.cache/tpl", u.Host, u.Path)
+
+	fmt.Println(trgt)
+
+	// check if already cloned
+	fi, err := os.Stat(trgt)
+	fmt.Println(fi.IsDir(), fi.Name())
+	if err == nil && fi.IsDir() {
+		r, err := git.PlainOpen(trgt)
+		if err != nil {
+			return "", err
 		}
+		w, err := r.Worktree()
+		if err != nil {
+			return "", err
+		}
+		if err := w.Pull(&git.PullOptions{RemoteName: "origin"}); err != nil {
+			return "", err
+		}
+		return trgt, nil
 	}
 
-	src, isDir := checkPathExistence(src)
-	if !isDir {
-		// handle archive
-		return handleArchive()
+	fmt.Println("making dir")
+
+	if err := os.MkdirAll(trgt, os.ModePerm); err != nil {
+		return "", err
+	}
+
+	fmt.Println(u.String())
+
+	_, err = git.PlainClone(trgt, false, &git.CloneOptions{
+		URL:      u.String(),
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		return "", err
+	}
+	return trgt, nil
+}
+
+func templateContext() map[string]interface{} {
+	return map[string]interface{}{
+		"env": envMap(),
 	}
 }
 
-func checkPathExistence(src string) (string, bool) {
-
-	// check if the input string refers to a dir.
-	fileInfo, err := os.Stat(src)
-	if err == nil && !fileInfo.IsDir() {
-		return src, nil
+func envMap() map[string]string {
+	m := make(map[string]string)
+	for _, e := range os.Environ() {
+		parts := strings.SplitN(e, "=", 2)
+		m[parts[0]] = parts[1]
 	}
-
-	// check if the input string refers to an archive.
-	for _, suffix := range archiveSuffixes {
-		archivePath := src + suffix
-		fileInfo, err := os.Stat(archivePath)
-		if err == nil && !fileInfo.IsDir() {
-			return handleArchive(archivePath)
-		}
-	}
-
-	// Check if the input string refers to a file or directory in the $TPL_PATH.
-	pathList := os.Getenv("TPL_PATH")
-	paths := strings.Split(pathList, string(os.PathListSeparator))
-	for _, path := range paths {
-		fullPath := filepath.Join(path, src)
-		fileInfo, err := os.Stat(fullPath)
-		if err == nil && !fileInfo.IsDir() {
-			return fullPath, nil
-		}
-		for _, suffix := range archiveSuffixes {
-			arhivePath := fullPath + suffix
-			fileInfo, err := os.Stat(arhivePath)
-			if err == nil && !fileInfo.IsDir() {
-				return handleArchive(arhivePath)
-			}
-		}
-	}
-
-	// Return an empty string and false if the file or directory is not found.
-	return "", false
+	return m
 }
